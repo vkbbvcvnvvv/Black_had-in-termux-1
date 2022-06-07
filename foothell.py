ef39fa6c844b48507ea58bd6321aec39fd819c88
vkbbvcvnvvv <94930978+vkbbvcvnvvv@users.noreply.github.com>
2022-06-07 19:11:08 +0200

    Add files via upload

---
+++ foothell.py
@@ -0,0 +1,55 @@
+import os
+import time
+os.system ("clear")
+print ("\033[1;32m||===================================================||")
+print ("\033[0;33m||             \033[0;31m  ___                                \033[0;33m ||")
+print ("\033[1;32m||       \033[0;31m __   _( _ )_ __                           \033[0;32m ||")
+print ("\033[0;33m||       \033[0;31m \ \ / / _ \ \/ /                          \033[0;33m ||")
+print ("\033[1;32m||        \033[0;31m \ V / (_) >  <                           \033[0;32m ||")
+print ("\033[0;33m||         \033[0;31m \_/ \___/_/\_/                          \033[0;33m ||")
+print ("\033[1;32m||                          \033[0;32mv8x                      ||")
+print ("\033[0;33m||===================================================||")
+print ("\033[0;31m||===========================================================================||")
+print ("\033[0;32m||We are anonymous we are little we do not forget we do not forgive Expect us\033[0;32m||")
+print ("\033[0;32m||G mail ==============>airc418@gmail.com                                    ||")
+print ("\033[0;32m||My organizer ltcjfyix76nt47uvtgbrosdc3fpft7ij4ht243yvbo6opg73i33oemqd.onion||")
+print ("\033[0;32m||my namer +201094286421                                                     ||")
+print ("\033[0;32m||my name Anonymous_various 8x attack                                        ||")
+print ("\033[0;31m||===========================================================================||")
+print ("\033[0;36m[1] \033[0;33mupgrade termux")
+name = input ("v8x☠️Chose Nuber : ")
+if name == "1" :
+#_______________
+     os.system("clear")
+     print ("\033[0;36m[1] \033[0;31minstall kali in termux")
+     time.sleep(0.3)
+#_______________
+     print ("\033[0;36m[2] \033[0;31minstall termux_desktop")
+     time.sleep(0.3)
+#_______________
+     print ("\033[0;36m[3] \033[0;31minstall termux_Black")
+     time.sleep(0.3)
+#_______________
+name = input ("Chose☠️Nuber : ")
+if name == "1" :
+     os.system ("apt update && apt upgrade -y")
+     os.system ("pkg install wget -y")
+     os.system ("termux-setup-storage")
+     os.system ("cd")
+     os.system ("wget -O install-nethunter-termux https://offs.ec/2MceZWr")
+     os.system ("chmod +x install-nethunter-termux")
+     os.system ("./install-nethunter-termux")
+#_______________
+if name == "2" :
+     os.system ("pkg upgrade && pkg install git -y")
+     os.system ("git clone --depth=1 https://github.com/adi1090x/termux-desktop.git")
+     os.system ("cd termux-desktop")
+     os.system ("chmod +x setup.sh")
+     os.system ("./setup.sh --install")
+#_______________
+if name == "3" :
+     os.system ("apt update && apt upgrade -y")
+     os.system ("apt install wget -y")
+     os.system ("cd")
+     os.system ("wget https://github.com/Hax4us/TermuxBlack/raw/master/install.sh")
+     
